3fd437ac2be55eb6efea90668e96bd16afcae430
Margaret <mrobinson526@gmail.com>
2021-09-09 08:49:56 -0500

    second commit

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
 <script type="text/javascript">
-    // Margareet G. Robinson
+    // Margaret G. Robinson
     // Create an array with 5 friends
-    let friends = ["Tony", "Dianna", "James", "Mary", "Joyce"];
-    for (let j=0; j<friends.length; j++) {
-        console.log(friends[j]);
-    }
-    function linesOfCode(number) {
-        for (let i=number; i>=0; i--){
-        let str="lines";
-        if (i==0) {
-            str="line";
-        }
-        console.log(i + " " + str + " of code in the file ", + i + " " str + " of code; " + friends[j] + " strikes one out, clears it all out, ")
-    }
-}
-    linesOfCode(99);
-
-
-
-
-
-
-
-
-
-
-
-    
+    const friendArray = [
+        {name: "Tony" },
+        {name: "Dianna" },
+        {name: "James" },
+        {name: "Mary" },
+        {name: "Joyce" }
+    ];
+    friendArray.array.forEach(friend, index => {
+        console.log(name);
+    });
+        for (let i=99; i<0; i--) {
+            let str="lines";
+            if (i==0) {
+                str="line";
+            };
+        console.log(i + " " + str + " of code in the file, " + i + " " + str + " of code:  " + friendArray[name] + " " + "strikes one out, clears it all out,");
+        };
 </script>
